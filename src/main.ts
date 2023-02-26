5a9a0d37c02ba177f72466c4e7826241a0643909
kristenbrann <tellkristenstuff@gmail.com>
2023-02-25 19:51:38 -0500

    Search interface: renders results and clicking result opens file (#10)
    * Search results panel shows when default search shows
    
    * wip
    
    * cleanup
    
    * functional
    
    * remove debuggers
    
    * remove logs

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { VectorStore } from "./VectorStore";
 import { OpenAIHandler } from "./OpenAIHandler"
 import { VIEW_TYPE_EXAMPLE, SemanticSearchView } from "./semanticSearchView";
 
-
 const randNum = () => Math.random() * (Math.round(Math.random()) * 2 - 1)
 const generateRandomVector = () => Array.from(new Array(1536), randNum)
 
@@ -20,23 +19,30 @@ export default class SemanticSearch extends Plugin {
 
 	viewActivated: boolean;
 
+	searchIconObserver: MutationObserver;
+
+	searchResultsObserver: MutationObserver;
+
+	searchTerm: string;
+
+	searchActive: boolean = false;
+
 	vectorStore: VectorStore;
 
 	openAIHandler: OpenAIHandler
 
 	async onload() {
 		await this.loadSettings();
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon("dice", "Activate view", () => {
-			this.activateView();
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		this.registerView(
 			VIEW_TYPE_EXAMPLE,
 			(leaf) => new SemanticSearchView(leaf)
-		);
+		)
+
+		this.app.workspace.onLayoutReady( () => {
+			this.registerSearchIconObserver()
+			this.registerSearchResultsObserver()
+		})
 
 		this.vectorStore = new VectorStore(this.app.vault)
 		this.vectorStore.isReady.then(async () => {
@@ -45,14 +51,14 @@ export default class SemanticSearch extends Plugin {
 			await this.vectorStore.updateVectorStore(files, this.openAIHandler.createEmbedding)
 
 			this.registerEvent(this.app.vault.on('delete', (file) => {
-				this.vectorStore.deleteByFilename(file.name)
+				this.vectorStore.deleteByFilePath(file.path)
 			}));
 
 			this.registerEvent(this.app.vault.on('create', (file) => {
 				if (file instanceof TFile) {
 					this.app.vault.read(file).then((fileContent) => {
-						this.openAIHandler.createEmbedding(`${file.name} ${fileContent}`).then((embedding) => {
-							this.vectorStore.addVector(file.name, embedding) 
+						this.openAIHandler.createEmbedding(`${file.path} ${fileContent}`).then((embedding) => {
+							this.vectorStore.addVector(file.path, embedding)
 						})
 
 					})
@@ -62,19 +68,19 @@ export default class SemanticSearch extends Plugin {
 			this.registerEvent(this.app.vault.on('modify', (file) => {
 				if (file instanceof TFile && file.name !== 'database2.json') {
 					this.app.vault.read(file).then((fileContent) => {
-						this.openAIHandler.createEmbedding(`${file.name} ${fileContent}`).then((embedding) => {
-							this.vectorStore.updateVectorByFilename(file.name, embedding) 
+						this.openAIHandler.createEmbedding(`${file.path} ${fileContent}`).then((embedding) => {
+							this.vectorStore.updateVectorByFilename(file.path, embedding)
 						})
 					})
 				}
 			}));
 
 			this.registerEvent(this.app.vault.on('rename', (file, oldPath) => {
-				this.vectorStore.deleteByFilename(oldPath)
+				this.vectorStore.deleteByFilePath(oldPath)
 				if (file instanceof TFile && file.name !== 'database2.json') {
 					this.app.vault.read(file).then((fileContent) => {
-						this.openAIHandler.createEmbedding(`${file.name} ${fileContent}`).then((embedding) => {
-							this.vectorStore.addVector(file.name, embedding) 
+						this.openAIHandler.createEmbedding(`${file.path} ${fileContent}`).then((embedding) => {
+							this.vectorStore.addVector(file.path, embedding)
 						})
 					})
 				}
@@ -94,38 +100,109 @@ export default class SemanticSearch extends Plugin {
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SemanticSearchSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	async activateView() {
-		if (this.viewActivated) {
-			this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
-			this.viewActivated = false;
-		} else {
-			this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+	showSemanticSearchPanel() {
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+		this.app.workspace.getLeftLeaf(true).setViewState({
+			type: VIEW_TYPE_EXAMPLE,
+			active: true,
+		}).then(() => {
+			const leavesOfType = this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)
+			if (leavesOfType.length > 0) {
+				this.app.workspace.revealLeaf(
+					leavesOfType[0]
+				);
+			}
 
-			await this.app.workspace.getLeftLeaf(true).setViewState({
-				type: VIEW_TYPE_EXAMPLE,
-				active: true,
-			});
+			this.viewActivated = true
+		})
+	}
 
-			this.app.workspace.revealLeaf(
-				this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0]
-			);
-			this.viewActivated = true;
-		}
+	hideSemanticSearchPanel() {
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+		this.viewActivated = false
+		this.searchTerm = ''
 	}
 
 	onunload() {
+		this.searchIconObserver.disconnect()
+		this.searchResultsObserver.disconnect()
+		this.app.workspace.getActiveViewOfType(SemanticSearchView)?.onClose()
 		this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+	}
+
+	registerSearchResultsObserver() {
+		const searchLeaf = this.app.workspace.getLeavesOfType('search')[0]
+		const searchContainerEl = searchLeaf.view.containerEl
+		const searchInfoEl = searchContainerEl.find(".search-info-container")
+		const searchResultContainerEl = searchContainerEl.find(".search-result-container")
+		this.searchResultsObserver = new MutationObserver(() => {
+			const searchInfoText = searchInfoEl.textContent
+			let searchTerm = ''
+			if (searchInfoText) {
+				if (searchInfoText.contains("Matches text: \"")) {
+					const startIndex = searchInfoText.indexOf("Matches text: \"")
+					searchTerm = searchInfoText.substring(startIndex + 13)
+					const split = searchTerm.split('"')
+					searchTerm = split[1]
+				} else if (searchInfoText.contains("Contains exact text: \"")) {
+					const startIndex = searchInfoText.indexOf("Contains exact text: \"")
+					searchTerm = searchInfoText.substring(startIndex + 13)
+					const split = searchTerm.split('"')
+					searchTerm = split[1]
+				}
+			}
+			const prevSearchTerm = this.searchTerm
+			if (searchTerm !== prevSearchTerm) {
+				this.searchTerm = searchTerm
+				this.searchForTerm(searchTerm).then((results) => {
+					const view = this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0].view as SemanticSearchView
+					view.updateSearchResults(results)
+				})
+			}
+		})
+		this.searchResultsObserver.observe(searchResultContainerEl, {
+			childList: true,
+			subtree: true
+		})
+	}
+
+	async searchForTerm(searchTerm: string): Promise<Array<string>> {
+		return this.openAIHandler.createEmbedding(searchTerm).then((embedding) => {
+			const results = this.vectorStore.getNearestVectors(embedding, 3)
+			return Array.from(results.keys())
+		})
+	}
+
+	registerSearchIconObserver() {
+		const appContainerEl = this.app.workspace.containerEl
+		const searchIconEl = appContainerEl.find("[aria-label=Search]")
+
+		/*
+		Watches the Search icon for `is-active` to be added or removed from the classes.
+		The presence of `is-active` indicates the search panel is visible and ours should be too.
+		*/
+		this.searchIconObserver = new MutationObserver((
+			mutations: MutationRecord[]
+		) => {
+			for(const mutation of mutations) {
+				if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+					const isActive = (mutation.target as HTMLElement).attributes.getNamedItem('class')?.value.contains('is-active')
+					if (isActive !== this.viewActivated) {
+						if (isActive) {
+							this.showSemanticSearchPanel()
+						} else {
+							this.hideSemanticSearchPanel()
+						}
+					}
+				}
+			}
+		})
+		this.searchIconObserver.observe(searchIconEl, {
+			attributes: true,
+			attributeFilter: [ 'class' ]
+		})
 	}
 
 	async loadSettings() {
