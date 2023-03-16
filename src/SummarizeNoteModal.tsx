9a86d9a74059dd428087290cc6e70716b29533a9
Garen Sahagian <g.s.sahagian@gmail.com>
2023-03-16 15:07:55 -0400

    getting rid of underscore

--- src/SummarizeNoteModal.tsx
+++ src/SummarizeNoteModal.tsx
@@ -46,7 +46,7 @@ export class SummarizeNoteModal extends Modal {
 		const summaryTitle = this.fileName.substring(0, this.fileName.length-3)
 		const noteRandomId = Math.floor(Math.random() * (100000 - 1) + 1);
 		const dateTime = (new Date().toISOString().split('T')[0])
-		const newNote = await this.app.vault.create(`/${summaryTitle}-VaultChat_Summary-${dateTime}-${noteRandomId}.md`, text)
+		const newNote = await this.app.vault.create(`/${summaryTitle}-VaultChat-Summary-${dateTime}-${noteRandomId}.md`, text)
 		await this.app.workspace.getLeaf().openFile(newNote)
 		this.app.workspace.activeEditor?.editor?.scrollTo(this.app.workspace.activeEditor?.editor?.lastLine())
 		this.app.workspace.activeEditor?.editor?.focus()
